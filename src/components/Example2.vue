8179d92965df6a788ae6e5381708301a12ab93a4
Francisco Igor <franciscoi@tbadigital.com>
2023-03-17 16:47:47 -0700

    Initial commit + Example Components

---
+++ src/components/Example2.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  props: {
+    msg: {
+        type: String
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<style>
+button{
+  border: 1px solid blue;
+}
+</style>
